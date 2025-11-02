c60fd5075ab90791aac9929a13fc8a89d34a2779
spriest487 <spriest487@googlemail.com>
2025-11-02 01:40:17 +0000

    Use individual padding bytes instead of arrays (generate less pointless array types)

--- frontend/src/codegen/metadata/ty/struct_def.rs
+++ frontend/src/codegen/metadata/ty/struct_def.rs
@@ -13,35 +13,27 @@ pub fn translate_struct_def(
     let name_path = translate_name(&struct_def.name, generic_ctx, lib);
 
     let mut fields = HashMap::new();
-    let mut pad_run = 0;
     let mut next_id = ir::FieldID(0);
     for member in lib.aligned_struct_members(struct_def) {
         match member {
             StructLayoutMember::Data { field_decl: member, decl_index, .. } => {
-                if pad_run > 0 {
-                    fields.insert(next_id, ir::StructFieldDef {
-                        name: None,
-                        ty: ir::Type::U8.array(pad_run),
-                    });
-                    pad_run = 0;
-                    next_id.0 += 1;
-                }
-
                 let name = member.idents[decl_index].to_string();
                 let ty = lib.translate_type(&member.ty, generic_ctx);
-                fields.insert(next_id, ir::StructFieldDef { name: Some(name), ty });
-                next_id.0 += 1;
+                
+                fields.insert(next_id, ir::StructFieldDef { 
+                    name: Some(name), 
+                    ty 
+                });
             }
 
             StructLayoutMember::PaddingByte => {
-                pad_run += 1;
+                fields.insert(next_id, ir::StructFieldDef {
+                    name: None,
+                    ty: ir::Type::U8,
+                });
             }
         }
-    }
-
-    if pad_run > 0 {
-        let pad_ty = ir::Type::U8.array(pad_run);
-        fields.insert(next_id, ir::StructFieldDef { name: None, ty: pad_ty });
+        next_id.0 += 1;
     }
 
     let identity = match struct_def.kind {
