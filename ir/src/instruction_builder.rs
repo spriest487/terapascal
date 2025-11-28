37d1f65978e5f59196ab6efc1542515cfc7b096e
spriest487 <spriest487@googlemail.com>
2025-11-28 23:47:55 +0000

    Util method for generating dtor bodies

--- ir/src/instruction_builder.rs
+++ ir/src/instruction_builder.rs
@@ -2,6 +2,7 @@ pub mod scope;
 pub mod util;
 mod dyn_array;
 mod invoker;
+mod object;
 
 use crate::BinOpInstruction;
 use crate::FieldID;
@@ -28,6 +29,7 @@ use std::sync::Arc;
 use terapascal_common::span::Span;
 pub use util::jmp_exists;
 pub use util::remove_empty_blocks;
+use crate::instruction_builder::object::gen_class_object_dtor_body;
 
 pub trait InstructionBuilder {
     fn emit(&mut self, instruction: Instruction);
@@ -874,6 +876,10 @@ pub trait InstructionBuilder {
         let temp_at_ptr = self.local_temp(ty.clone().temp_ref());
         self.make_ref(temp_at_ptr.clone(), at);
         temp_at_ptr.value()
+    }
+    
+    fn gen_class_object_dtor_body(&mut self, class_id: TypeDefID, self_param: LocalID) -> bool {
+        gen_class_object_dtor_body(self, class_id, self_param)
     }
 
     fn gen_dyn_array_dtor_body(&mut self, self_param: LocalID, element_type: &Type) {
---
+++ ir/src/instruction_builder/object.rs
@@ -0,0 +1,38 @@
+﻿use crate::instruction_builder::InstructionBuilder;
+use crate::LocalID;
+use crate::TypeDefID;
+
+pub fn gen_class_object_dtor_body<B>(builder: &mut B, class_id: TypeDefID, self_param: LocalID) -> bool
+where
+    B: InstructionBuilder + ?Sized
+{
+    let class_ty = class_id.to_class_ptr_type();
+
+    let class_pretty_name = builder.metadata().pretty_ty_name(&class_ty).into_owned();
+
+    // we have to do this loop manually, because the "self" reference in a class method is
+    // meant to be immutable (it's illegal to reference it even via an immutable reference in CIL).
+    // using visit_deep on self would use references!
+    let class_struct_def = builder.metadata()
+        .get_struct_def(class_id)
+        .unwrap_or_else(|| {
+            panic!("gen_class_object_dtor_body: missing definition for resource struct of {class_pretty_name}", )
+        })
+        .clone();
+    
+    let mut released_any = false;
+
+    for (field_id, field_def) in class_struct_def.fields {
+        if !field_def.ty.contains_any_object_refs(builder.metadata()) {
+            continue;
+        }
+
+        let field_ref = builder.local_temp(field_def.ty.clone().temp_ref());
+        builder.field(field_ref, self_param, class_ty.clone(), field_id);
+        released_any |= builder.release_deep(field_ref.to_deref(), &field_def.ty);
+
+        assert!(released_any, "if contains_any_object_refs returns true, release_deep should find a reference to release")
+    }
+    
+    released_any
+}
