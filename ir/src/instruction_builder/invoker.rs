04393d6ee61e54662a109d037bc7d6f16d7bc985
spriest487 <spriest487@googlemail.com>
2025-12-16 02:19:23 +0000

    Tidying up + fix codegen bug reusing same local

--- ir/src/instruction_builder/invoker.rs
+++ ir/src/instruction_builder/invoker.rs
@@ -90,127 +90,105 @@ where
     let arg_ref = builder.local_temp(Type::any().temp_ref());
 
     for param_index in 0..func_sig.param_tys.len() {
+        let param_ty = &func_sig.param_tys[param_index];
+        let expect_object_id = match param_ty {
+            Type::Object(id) | Type::WeakObject(id) => id.clone(),
+
+            Type::TempRef(deref_type) => ObjectID::Box(deref_type.clone()),
+
+            value_type => ObjectID::Box(Rc::new(value_type.clone())),
+        };
+
+        // fetch the arg ref
+        // if a self-arg was passed, use that for parameter 0 and offset remaining args
+        builder.if_then_else(
+            has_self_arg.clone(),
+            |builder| {
+                if param_index == 0 {
+                    builder.comment(format!("param {}: self arg", param_index));
+                    builder.mov(arg_ref, self_arg_ref);
+                } else {
+                    let arg_index = param_index as i32 - 1;
+                    let index_val = Value::LiteralI32(arg_index);
+
+                    builder.comment(format!("param {}: arg {}", param_index, arg_index));
+                    builder.element(arg_ref, args_arg, index_val, arg_array_type.clone());
+                }
+            },
+            |builder| {
+                builder.comment(format!("param {}: arg {}", param_index, param_index));
+                builder.element(
+                    arg_ref,
+                    args_arg,
+                    Value::LiteralI32(param_index as i32),
+                    arg_array_type.clone(),
+                );
+            },
+        );
+
+        builder.comment(format!(
+            "exit with error if arg {param_index} is not {}",
+            expect_object_id
+                .to_object_type()
+                .to_pretty_string(builder.metadata())
+        ));
+
         builder.local_begin();
         {
-            let param_ty = &func_sig.param_tys[param_index];
-            let expect_object_id = match param_ty {
-                Type::Object(id) | Type::WeakObject(id) => id.clone(),
+            let arg_is_valid = builder.local_temp(Type::Bool);
+            let arg_is_invalid = builder.local_temp(Type::Bool);
 
-                Type::TempRef(deref_type) => ObjectID::Box(deref_type.clone()),
-
-                value_type => ObjectID::Box(Rc::new(value_type.clone())),
-            };
-
-            // fetch the arg ref
-            // if a self-arg was passed, use that for parameter 0 and offset remaining args
-            builder.if_then_else(
-                has_self_arg.clone(),
-                |builder| {
-                    if param_index == 0 {
-                        builder.comment(format!("param {}: self arg", param_index));
-                        builder.mov(arg_ref, self_arg_ref);
-                    } else {
-                        let arg_index = param_index as i32 - 1;
-                        let index_val = Value::LiteralI32(arg_index);
-
-                        builder.comment(format!("param {}: arg {}", param_index, arg_index));
-                        builder.element(arg_ref, args_arg, index_val, arg_array_type.clone());
-                    }
-                },
-                |builder| {
-                    builder.comment(format!("param {}: arg {}", param_index, param_index));
-                    builder.element(
-                        arg_ref,
-                        args_arg,
-                        Value::LiteralI32(param_index as i32),
-                        arg_array_type.clone(),
-                    );
-                },
-            );
-
-            builder.comment(format!(
-                "exit with error if arg {param_index} is not {}",
-                expect_object_id
-                    .to_object_type()
-                    .to_pretty_string(builder.metadata())
-            ));
-
-            builder.local_begin();
-            {
-                let arg_is_valid = builder.local_temp(Type::Bool);
-                let arg_is_invalid = builder.local_temp(Type::Bool);
-
-                builder.class_is(arg_is_valid, arg_ref.to_deref(), expect_object_id);
-                builder.not(arg_is_invalid, arg_is_valid);
-                builder.jmpif(exit_error_label, arg_is_invalid);
-            }
-            builder.local_end();
-
-            match param_ty {
-                // object types: downcast and pass the arg directly
-                Type::Object(..) | Type::WeakObject(..) => {
-                    builder.comment("object param: copy the argument");
-                    let call_arg = builder.local_temp(param_ty.clone());
-                    builder.cast(call_arg, arg_ref.to_deref(), param_ty.clone());
-
-                    call_args.push(call_arg.value());
-                },
-
-                Type::TempRef(deref_type) => {
-                    // unbox...
-                    let value_type = deref_type.as_ref();
-                    let box_type = value_type.clone().boxed();
-
-                    let new_val_ref = builder.local_temp(value_type.clone().temp_ref());
-
-                    builder.local_begin();
-                    {
-                        let arg_box = builder.local_temp(box_type.clone());
-                        builder.cast(arg_box, arg_ref.to_deref(), box_type.clone());
-
-                        let arg_val = builder.element_to_val(
-                            arg_box,
-                            Value::I32_0,
-                            value_type.clone(),
-                            box_type.clone(),
-                        );
-
-                        builder.comment("ref param: clone the boxed argument into a new box");
-                        let new_box = builder.local_temp(box_type.clone());
-
-                        builder.new_box(new_box, value_type.clone(), false);
-                        builder.element(new_val_ref, new_box, Value::I32_0, box_type.clone());
-                        builder.mov(new_val_ref.to_deref(), arg_val);
-
-                        builder.release(arg_ref.to_deref(), false, Ref::Discard);
-                        builder.cast(arg_ref.to_deref(), new_box, Type::any());
-                    }
-                    builder.local_end();
-
-                    // pass the ref to the new box's element (func expects a ref)
-                    call_args.push(new_val_ref.value());
-                },
-
-                value_type => {
-                    // unbox the arg and pass that
-                    let unboxed_val = builder.local_temp(value_type.clone());
-
-                    builder.comment("value param: unbox the argument");
-                    builder.local_begin();
-                    {
-                        let box_type = value_type.clone().boxed();
-                        let arg_box = builder.local_temp(box_type.clone());
-
-                        builder.cast(arg_box, arg_ref.to_deref(), box_type.clone());
-                        builder.element_val(unboxed_val, arg_box, Value::I32_0, value_type.clone(), box_type);
-                    }
-                    builder.local_end();
-
-                    call_args.push(unboxed_val.value());
-                },
-            }
+            builder.class_is(arg_is_valid, arg_ref.to_deref(), expect_object_id);
+            builder.not(arg_is_invalid, arg_is_valid);
+            builder.jmpif(exit_error_label, arg_is_invalid);
         }
         builder.local_end();
+
+        match param_ty {
+            // object types: downcast and pass the arg directly
+            Type::Object(..) | Type::WeakObject(..) => {
+                builder.comment("object param: copy the argument");
+                let call_arg = builder.local_temp(param_ty.clone());
+                builder.cast(call_arg, arg_ref.to_deref(), param_ty.clone());
+
+                call_args.push(call_arg.value());
+            },
+
+            Type::TempRef(deref_type) => {
+                // unbox...
+                let value_type = deref_type.as_ref();
+
+                let new_val_ref = builder.local_temp(value_type.clone().temp_ref());
+
+                builder.comment("ref param: clone the boxed argument into a new box");
+                builder.local_begin();
+                {
+                    gen_ref_arg_boxed_value(builder, new_val_ref, arg_ref, value_type);
+                }
+                builder.local_end();
+
+                // pass the ref to the new box's element (func expects a ref)
+                call_args.push(new_val_ref.value());
+            },
+
+            value_type => {
+                // unbox the arg and pass that
+                let unboxed_val = builder.local_temp(value_type.clone());
+
+                builder.comment("value param: unbox the argument");
+                builder.local_begin();
+                {
+                    let box_type = value_type.clone().boxed();
+                    let arg_box = builder.local_temp(box_type.clone());
+
+                    builder.cast(arg_box, arg_ref.to_deref(), box_type.clone());
+                    builder.element_val(unboxed_val, arg_box, Value::I32_0, value_type.clone(), box_type);
+                }
+                builder.local_end();
+
+                call_args.push(unboxed_val.value());
+            },
+        }
     }
 
     if func_sig.return_ty == Type::Nothing {
@@ -221,27 +199,7 @@ where
         let call_result = builder.local_temp(func_sig.return_ty.clone());
         builder.call(func_id, call_args, Some(call_result.to_ref()));
 
-        if func_sig.return_ty.is_object() {
-            // result is an object: replace the result ref
-            builder.cast(RETURN_REF, call_result, Type::any());
-        } else {
-            // box the result
-            let result_box_type = func_sig.return_ty.clone().boxed();
-
-            let result_box = builder.local_temp(result_box_type.clone());
-            let result_box_value_ref = builder.local_temp(func_sig.return_ty.clone().temp_ref());
-
-            builder.new_box(result_box, func_sig.return_ty.clone(), false);
-            builder.element(
-                result_box_value_ref,
-                result_box,
-                Value::LiteralI32(0),
-                result_box_type,
-            );
-            builder.mov(result_box_value_ref.to_deref(), call_result);
-
-            builder.cast(RETURN_REF, result_box, Type::any());
-        }
+        gen_box_result(builder, &func_sig.return_ty, call_result.value());
     }
 
     builder.mov(result_code_ref_arg.to_deref(), Value::I32_0);
@@ -252,4 +210,62 @@ where
     builder.mov(RETURN_REF, Value::LiteralNull);
 
     builder.label(exit_label);
+}
+
+// for ref args: 
+// the arg value is expected to be a box of the value. we clone this box into a new box of the same
+// type (since the function may modify the value, it can't be allowed to modify the original box),
+// and return a ref (the actual arg) to the new box's value
+fn gen_ref_arg_boxed_value<B>(builder: &mut B, ref_out: LocalID, boxed_arg: LocalID, value_type: &Type)
+where
+    B: InstructionBuilder + ?Sized,
+{
+    let box_type = value_type.clone().boxed();
+
+    let arg_box = builder.local_temp(box_type.clone());
+    builder.cast(arg_box, boxed_arg.to_deref(), box_type.clone());
+
+    let boxed_value = builder.element_to_val(
+        arg_box,
+        Value::I32_0,
+        value_type.clone(),
+        box_type.clone(),
+    );
+
+    let new_box = builder.local_temp(box_type.clone());
+
+    builder.new_box(new_box, value_type.clone(), false);
+    builder.element(ref_out, new_box, Value::I32_0, box_type.clone());
+    builder.mov(ref_out.to_deref(), boxed_value);
+
+    builder.release(boxed_arg.to_deref(), false, Ref::Discard);
+    builder.cast(boxed_arg.to_deref(), new_box, Type::any());
+}
+
+fn gen_box_result<B>(builder: &mut B, return_type: &Type, call_result: Value)
+where
+    B: InstructionBuilder + ?Sized,
+{
+    if return_type.is_object() {
+        // result is an object: replace the result ref
+        builder.cast(RETURN_REF, call_result, Type::any());
+        return;
+    }
+
+    // box the result
+    let result_box_type = return_type.clone().boxed();
+
+    let result_box = builder.local_temp(result_box_type.clone());
+    let result_box_value_ref = builder.local_temp(return_type.clone().temp_ref());
+
+    builder.new_box(result_box, return_type.clone(), false);
+    builder.element(
+        result_box_value_ref,
+        result_box,
+        Value::LiteralI32(0),
+        result_box_type,
+    );
+    builder.mov(result_box_value_ref.to_deref(), call_result);
+
+    builder.cast(RETURN_REF, result_box, Type::any());
 }
