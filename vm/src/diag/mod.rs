4eeeff09354a161c76fffc79d9d5c35e256b718a
spriest487 <spriest487@googlemail.com>
2026-03-10 18:17:45 +0000

    Fix build error with diag enabled

--- vm/src/diag/mod.rs
+++ vm/src/diag/mod.rs
@@ -5,7 +5,7 @@ use serde::Serialize;
 #[cfg(not(feature = "debug_server"))]
 pub use self::impl_null::*;
 
-#[cfg(feature="debug_server")]
+#[cfg(feature = "debug_server")]
 pub use self::impl_rouille::*;
 
 #[derive(Debug, Clone, Serialize)]
@@ -14,6 +14,7 @@ pub struct DiagnosticOutput {
     pub heap_stats: HeapStats,
 }
 
+#[cfg(not(feature = "debug_server"))]
 mod impl_null {
     use crate::diag::DiagnosticOutput;
 
@@ -33,80 +34,82 @@ mod impl_null {
 
         pub fn shutdown(self) {}
     }
+}
 
-    #[cfg(feature = "debug_server")]
-    mod impl_rouille {
-        use rouille::Response;
-        use rouille::Server;
-        use std::net::Ipv4Addr;
-        use std::net::SocketAddr;
-        use std::sync::mpsc::channel;
-        use std::sync::mpsc::Receiver;
-        use std::sync::mpsc::Sender;
-        use std::sync::Arc;
-        use std::sync::Mutex;
-        use std::thread::JoinHandle;
-        use super::DiagnosticOutput;
+#[cfg(feature = "debug_server")]
+mod impl_rouille {
+    use super::DiagnosticOutput;
+    use rouille::Response;
+    use rouille::Server;
+    use std::net::Ipv4Addr;
+    use std::net::SocketAddr;
+    use std::sync::Arc;
+    use std::sync::Mutex;
+    use std::sync::mpsc::Receiver;
+    use std::sync::mpsc::Sender;
+    use std::sync::mpsc::channel;
+    use std::thread::JoinHandle;
 
-        #[derive(Debug)]
-        pub struct DiagnosticWorker {
-            join_handle: JoinHandle<()>,
-            stop_server: Sender<()>,
+    #[derive(Debug)]
+    pub struct DiagnosticWorker {
+        join_handle: JoinHandle<()>,
+        stop_server: Sender<()>,
 
-            diag_send: Sender<DiagnosticOutput>,
-            ready_recv: Receiver<()>,
+        diag_send: Sender<DiagnosticOutput>,
+        ready_recv: Receiver<()>,
+    }
+
+    impl DiagnosticWorker {
+        pub fn new(port: u16) -> Option<Self> {
+            let (ready_send, ready_recv) = channel::<()>();
+            let (diag_send, diag_recv) = channel::<DiagnosticOutput>();
+
+            let diag_recv = Arc::new(Mutex::new(diag_recv));
+
+            let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), port);
+
+            let server = Server::new(addr, move |_request| {
+                let ready_send = ready_send.clone();
+                if let Err(err) = ready_send.send(()) {
+                    eprintln!("[diag] failed to read from signalling channel");
+                    return Response::text(err.to_string()).with_status_code(500);
+                }
+
+                let next_diag = diag_recv.lock().unwrap().recv().unwrap();
+                Response::json(&next_diag)
+            })
+            .map_err(|err| {
+                eprintln!("[diag] failed to start server thread: {err}");
+            })
+            .ok()?;
+
+            let (join_handle, stop_server) = server.stoppable();
+
+            Some(Self {
+                join_handle,
+                stop_server,
+                diag_send,
+                ready_recv,
+            })
         }
 
-        impl DiagnosticWorker {
-            pub fn new(port: u16) -> Option<Self> {
-                let (ready_send, ready_recv) = channel::<()>();
-                let (diag_send, diag_recv) = channel::<DiagnosticOutput>();
+        pub fn update<DiagSource>(&self, diag: DiagSource)
+        where
+            DiagSource: FnOnce() -> DiagnosticOutput,
+        {
+            if self.ready_recv.try_recv().is_ok() {
+                let output = diag();
 
-                let diag_recv = Arc::new(Mutex::new(diag_recv));
-
-                let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), port);
-
-                let server = Server::new(addr, move |_request| {
-                    let ready_send = ready_send.clone();
-                    if let Err(err) = ready_send.send(()) {
-                        eprintln!("[diag] failed to read from signalling channel");
-                        return Response::text(err.to_string()).with_status_code(500);
-                    }
-
-                    let next_diag = diag_recv.lock().unwrap().recv().unwrap();
-                    Response::json(&next_diag)
-                }).map_err(|err| {
-                    eprintln!("[diag] failed to start server thread: {err}");
-                }).ok()?;
-
-                let (join_handle, stop_server) = server.stoppable();
-
-                Some(Self {
-                    join_handle,
-                    stop_server,
-                    diag_send,
-                    ready_recv,
-                })
-            }
-
-            pub fn update<DiagSource>(&self, diag: DiagSource)
-            where
-                DiagSource: FnOnce() -> DiagnosticOutput,
-            {
-                if self.ready_recv.try_recv().is_ok() {
-                    let output = diag();
-
-                    if !self.diag_send.send(output).is_ok() {
-                        eprintln!("[diag] channel closed");
-                    }
+                if !self.diag_send.send(output).is_ok() {
+                    eprintln!("[diag] channel closed");
                 }
             }
+        }
 
-            pub fn shutdown(self) {
-                if self.stop_server.send(()).is_ok() {
-                    if let Err(..) = self.join_handle.join() {
-                        eprintln!("[diag] failed to join diagnostics thread");
-                    }
+        pub fn shutdown(self) {
+            if self.stop_server.send(()).is_ok() {
+                if let Err(..) = self.join_handle.join() {
+                    eprintln!("[diag] failed to join diagnostics thread");
                 }
             }
         }
