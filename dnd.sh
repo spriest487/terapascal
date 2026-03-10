55d0924ba2422f753d2f05f231587f1e6eb912ba
spriest487 <spriest487@googlemail.com>
2026-03-10 18:17:12 +0000

    Add dnd.sh

---
+++ dnd.sh
@@ -0,0 +1,5 @@
+dotnet build backend_cil
+cargo run --features backend-cil -- demos/$1.tpas -a cil --debug --print-stage=ir > target/$1.txt
+cargo run --features backend-cil -- demos/$1.tpas -a cil --debug -o target/$1.lib
+cargo run --features backend-cil -- demos/$1.tpas -a cil --debug -o target/$1.dll
+dotnet target/$1.dll
