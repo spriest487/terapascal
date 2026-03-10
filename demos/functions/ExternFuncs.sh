88d5601b3a9fbacbc377703c75ed4456a1effa8a
spriest487 <spriest487@googlemail.com>
2026-03-10 01:28:22 +0000

    Update test SO build script

--- demos/functions/ExternFuncs.sh
+++ demos/functions/ExternFuncs.sh
@@ -1,1 +1,2 @@
-clang -shared ExternFuncs.c -o ExternFuncs.dll
+clang -fPIC -shared ExternFuncs.c -g -O0 -o ../../target/debug/libExternFuncs.so
+clang -fPIC -shared ExternFuncs.c -o ../../target/release/libExternFuncs.so
